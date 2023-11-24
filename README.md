f20ed9b81d72c69d35978b0ac39813ebf3e7df83
Erika Martínez <erikamp1112@gmail.com>
2023-11-24 01:56:14 +0100

    Version 1.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# Proyecto DWES
+## Autor
+Erika Martínez Pérez
+## Requisitos minimos del desarrollo
+- [Apache2.4.52]
+- [MySQL8.0.35]
+- [PHP8.1]
+- [XDebug3.1.2]
+## Descripcion
+Documentacion de la asignatura desarrollo web en entorno cliente / DWEC
+## Funcionalidades
+## Entorno de desarrollo
+[http://daw202.isauces.local/202DWECProyectoDWEC/indexProyectoDWEC.php]
+(Entorno de desarrollo de Erika)
+## Entorno de explotacion
+[http://daw202.ieslossauces.es/202DWECProyectoDWEC/indexProyectoDWEC.php]
+(Entorno de explotación de Erika)
---
+++ indexProyectoDWEC.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="/webroot/css/style.css">
+        <title>DWES</title>
+    </head>
+    <body>
+        <header>
+            <h1>Desarrollo web en entorno cliente</h1>
+        </header>
+        <p>Profesor: Alberto Bahillo Fernandez</p>
+        <footer>
+            <p>2023-2024 © Todos los derechos reservados. <a href="../index.html">Erika Martínez Pérez</a></p>
+        </footer>
+    </body>
+</html>
